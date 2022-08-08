61bcbbbb1ced4351f5391e157ffede18ff56cf94
Gunjan Dalwadi <85817658+gunjan2001@users.noreply.github.com>
2022-08-08 18:04:37 +0530

    Add files via upload

---
+++ build/precache-manifest.d17bd7fe1b340559bc84259ae217173b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51de6b1a1d403ee12b27",
+    "url": "/static/css/main.439f92bf.chunk.css"
+  },
+  {
+    "revision": "51de6b1a1d403ee12b27",
+    "url": "/static/js/main.51de6b1a.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "84c6e94bac325a981da8",
+    "url": "/static/js/2.84c6e94b.chunk.js"
+  },
+  {
+    "revision": "769c94daf783692f2c65",
+    "url": "/static/js/runtime~main.769c94da.js"
+  },
+  {
+    "revision": "24acb43f00c062abae33ac7e8680b033",
+    "url": "/index.html"
+  }
+];
